040075987f42830f1f61eac7bc55eabaae98d754
sushil50889 <mandi.sushil@gmail.com>
2018-06-18 12:07:34 +0530

    update google client id and secret

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ module.exports = {
     JWT_SECRET: 'testSecret',
     oauth: {
       google: {
-        clientID: '785120135299-i8mhf6e51k6jvvtt05c4rpl345jmp71i.apps.googleusercontent.com',
-        clientSecret: 'Z55U21mmeqKHq7gMwti_NtuD'
+        clientID: '785120135299-6ji63dh9ukqk5i0neg3nqkob3g1a3bs9.apps.googleusercontent.com',
+        clientSecret: 'd6PRUD83VR10fw__Ja1pCvyv'
       }      
     }
   };
